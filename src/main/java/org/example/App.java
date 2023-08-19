4182da7166fb59454acc83737e1cee4383ee376b
Muhammad Acla Alamsyah Nurdin Hamzah <acla.hamzah@shopee.com>
2023-08-20 02:51:28 +0700

    feat: add base

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.EventQueue;
+/**
+ * Hello world!
+ *
+ */
+public class App extends JFrame
+{
+    public App() {
+        initUI();
+    }
+
+    private void initUI() {
+        add(new Board());
+        setSize(250, 200);
+        setTitle("Application");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main( String[] args )
+    {
+        EventQueue.invokeLater(() -> {
+            App ex = new App();
+            ex.setVisible(true);
+        });
+    }
+}
---
+++ src/main/java/org/example/Board.java
@@ -0,0 +1,7 @@
+package org.example;
+
+import javax.swing.JPanel;
+
+public class Board extends JPanel{
+    public Board() {}
+}
